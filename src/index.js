c40952cc1d7c1a157dd32a1dd1b71df92e4789a0
chintanghevariya <73758067+chintanghevariya@users.noreply.github.com>
2021-12-02 15:34:37 -0500

    assignment 2 - frontend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ReactApp from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
